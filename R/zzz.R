16aae2db4019eb6b4913f988bd5a51e51b0f63a9
Martin Morgan <mtmorgan@fhcrc.org>
2014-09-02 14:00:50 -0700

    R-level package infrastructure

---
+++ R/zzz.R
@@ -0,0 +1,26 @@
+pkgconfig <-
+    function(opt = c("PKG_CFLAGS", "PKG_LIBS"))
+{
+    ## path <- system.file("lib", package="Rhtslib", mustWork=TRUE)
+    ## if (nzchar(.Platform$r_arch)) {
+    ##     arch <- sprintf("/%s", .Platform$r_arch)
+    ## } else {
+    ##     arch <- ""
+    ## }
+    ## patharch <- paste0(path, arch)
+
+    ## result <- switch(match.arg(opt), PKG_CFLAGS={
+    ##     sprintf('-I"%s"', system.file("include", package="Rhtslib"))
+    ## }, PKG_LIBS={
+    ##     sprintf('-L"%s" -Wl,-rpath,"%s" -lhts -lz', patharch, patharch)
+    ## })
+
+    ## cat(result)
+}
+
+.onAttach <-
+    function(...)
+{
+    ## vers <- .Call("Rhtslib_htslib_version", PACKAGE="Rhtslib")
+    ## packageStartupMessage("Rhtslib htslib version ", vers)
+}
